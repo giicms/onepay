b1403fe5cb6683b44dbf4cc8333b49bfc828de6a
ZA Knight <letrunghoa90@gmail.com>
2015-12-24 14:33:30 +0700

    Update test

---
+++ src/OnepayServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+namespace zaknight\onepay;
+use illuminate\support\ServiceProvider
+
+class OnepayServiceProvider extends ServiceProvider
+{
+  public function boot()
+  {
+
+  }
+  public function register()
+  {
+
+  }
+}
